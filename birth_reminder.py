654cb085556b86cdb4496e789e282da92c2be52a
VitaliyLitvak <vitaliy.litvak@gmail.com>
2023-06-17 00:45:12 +0200

    Update birth_reminder.py

--- birth_reminder.py
+++ birth_reminder.py
@@ -2,14 +2,14 @@ from datetime import datetime, timedelta, date
 from rich import print
 
 
-users = [{'name': 'Христина Зінчук', 'birthday': '2023-06-19'},
-         {'name': 'Валентин Копитко', 'birthday': '1989-06-19'},
+users = [{'name': 'Христина Зінчук', 'birthday': '2023-06-17'},
+         {'name': 'Валентин Копитко', 'birthday': '1989-06-18'},
          {'name': 'Нестор Авраменко', 'birthday': '1954-06-20'},
          {'name': 'Мирон Філіпенко', 'birthday': '1936-06-21'},
-         {'name': 'Мартин Засенко', 'birthday': '1923-06-13'},
+         {'name': 'Мартин Засенко', 'birthday': '1923-06-19'},
          {'name': 'Вікторія Каденюк', 'birthday': '1985-06-16'},
          {'name': 'Веніямин Ґалаґан', 'birthday': '1981-06-19'},
-         {'name': 'Святослав Єфименко', 'birthday': '1965-06-23'},
+         {'name': 'Святослав Єфименко', 'birthday': '1965-06-26'},
          {'name': 'Ірина Дерегус', 'birthday': '1946-06-25'},
          {'name': 'Лариса Лупій', 'birthday': '2020-06-24'}]
 DAYS = {'Monday': [],
@@ -20,11 +20,15 @@ DAYS = {'Monday': [],
         }
 TODAY = date.today()
 CURRENT_YEAR = date.today().year
+
     
 def birth_reminder(users):
-    week_start = TODAY - timedelta(days=TODAY.weekday()-7)
+    week_start = TODAY - timedelta(days=TODAY.weekday()-5)
     week_end = week_start + timedelta(days=6)
     delete_days = []
+    print(week_start)
+    print(week_end)
+    # Пошук потрібних юзерів
     for user in users:
         user_birthday = datetime.strptime(user['birthday'], '%Y-%m-%d').date()
         age = CURRENT_YEAR - user_birthday.year
@@ -34,11 +38,14 @@ def birth_reminder(users):
                 DAYS[user_birthday.strftime('%A')].append([user['name'], age])
             else:
                 DAYS['Monday'].append([user['name'], age])
+    # Видалення пустих днів    
     for day in DAYS:
         if not DAYS[day]:
             delete_days.append(day)
     for day in delete_days:
         del DAYS[day]
+        
     return DAYS
+    
     
 print(birth_reminder(users))
