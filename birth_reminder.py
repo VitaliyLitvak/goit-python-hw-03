f0852ff1e6e8549ca80ece0fae62b98ae1100061
VitaliyLitvak <vitaliy.litvak@gmail.com>
2023-06-18 00:38:39 +0200

    Updated

--- birth_reminder.py
+++ birth_reminder.py
@@ -12,36 +12,26 @@ users = [{'name': 'Христина Зінчук', 'birthday': '2023-06-17'},
          {'name': 'Святослав Єфименко', 'birthday': '1965-06-26'},
          {'name': 'Ірина Дерегус', 'birthday': '1946-06-25'},
          {'name': 'Лариса Лупій', 'birthday': '2020-06-24'}]
-DAYS = {'Monday': [],
-        'Tuesday': [],
-        'Wednesday': [],
-        'Thursday': [],
-        'Friday': []
-        }
-TODAY = date.today()
-CURRENT_YEAR = date.today().year
 
     
 def birth_reminder(users):
-    week_start = TODAY - timedelta(days=TODAY.weekday()-5)
+    current_year = date.today().year
+    today = date.today()
+    week_start = today - timedelta(days=today.weekday()-5)
     week_end = week_start + timedelta(days=6)
     delete_days = []
+    print(week_start, week_end)
+    DAYS = {}
     # Пошук потрібних юзерів
     for user in users:
         user_birthday = datetime.strptime(user['birthday'], '%Y-%m-%d').date()
-        age = CURRENT_YEAR - user_birthday.year
-        user_birthday = user_birthday.replace(year=CURRENT_YEAR)
+        age = current_year - user_birthday.year
+        user_birthday = user_birthday.replace(year=current_year)
         if week_start <= user_birthday <= week_end:
             if user_birthday.weekday() < 5:
-                DAYS[user_birthday.strftime('%A')].append([user['name'], age])
+                DAYS.setdefault(user_birthday.strftime('%A'), []).append([user['name'], age])
             else:
-                DAYS['Monday'].append([user['name'], age])
-    # Видалення пустих днів    
-    for day in DAYS:
-        if not DAYS[day]:
-            delete_days.append(day)
-    for day in delete_days:
-        del DAYS[day]
+                DAYS.setdefault('Monday', []).append([user['name'], age])
         
     return DAYS
     
